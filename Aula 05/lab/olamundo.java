31045628b412fcf248b59e096a9254d9b01f1437
GLEISON TIAGO MARTINS DE ARAUJO <gleisontiago@Outlook.com>
2022-03-02 09:42:52 -0300

    Iniciando o curso

---
+++ Aula 05/lab/olamundo.java
@@ -0,0 +1,7 @@
+class olamundo {
+	
+	public static void main(String[] args){
+		System.out.println("Olá Mundo!");
+
+	}
+}
---
+++ Aula 06/Lab/Argumentos2.java
@@ -0,0 +1,8 @@
+class Argumentos2 {
+	
+	public static void main (String[] args){
+
+		System.out.println("Olá Mundo, você digitou: " + args [0]);
+
+	}
+}
